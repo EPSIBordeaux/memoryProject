12d41502e4bf7e38b41be5096c504b715b1ce700
Sylvain METAYER <contact@sylvainmetayer.fr>
2018-11-27 16:40:25 +0100

    feat: add button

--- components/StepComponent.js
+++ components/StepComponent.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
 import { Text } from 'react-native'
+import { Button } from 'react-native'
+
 
 export default class StepComponent extends React.Component {
     render() {
         return [
             <Text stepId={this.props.id}>{this.props.step.content}</Text>,
             this.props.step.answers.map(function (object, i) {
-                return <Text stepId={object.next} key={object.next}> {i+1} - {object.action} </Text>;
+                return <Button
+                    onPress={() => {
+                        console.log(object.next)
+                    }}
+                    title={`${i + 1} - ${object.action}`}
+                    color="#841584"
+                    stepId={object.next}
+                    key={object.next}
+                    accessibilityLabel="Learn more about this purple button"
+                />
             })
         ]
     }
