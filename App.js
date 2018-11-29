30d1ab0f12efb4e94fcf9e7477047e3353e78aca
JulietteR <juliette@riviere.im>
2018-11-29 10:42:50 +0100

    FEAT : Mise en place de AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, StyleSheet, View, Text } from 'react-native'
+import { Button, StyleSheet, View, Text, AsyncStorage } from 'react-native'
 
 import DisplayComponent from './components/DisplayComponent'
 import ActionComponent from './components/ActionComponent'
@@ -8,56 +8,69 @@ const data = require("./assets/story/story.json")
 
 export default class App extends React.Component {
 
-  constructor(props) {
+    constructor(props) {
 
-    super(props);
+        super(props);
 
-    this.state = {
-      currentId: 0
+        this.state = {
+            currentId: 0
+        }
+
+        this.handler = this.handler.bind(this)
     }
 
-    this.handler = this.handler.bind(this)
-  }
+    handler(stepId) {
+        this.setState({
+            currentId: stepId
+        })
+    }
 
-  handler(stepId) {
-    this.setState({
-      currentId: stepId
-    })
-  }
+    getCurrentId() {
+        return this.state.currentId;
+    }
 
-  getCurrentId() {
-    return this.state.currentId;
-  }
+    findById(id) {
+        return data.steps[id];
+    }
 
-  findById(id) {
-    return data.steps[id];
-  }
+    state = {
+        'progressionStep': ''
+    }
 
-  render() {
+    componentDidMount = () => {
+        AsyncStorage.setItem('progressionStep', 'oust');
+        AsyncStorage.getItem('progressionStep').then((value) => this.setState({ 'progressionStep': value }))
+    }
+    setProgressionStep = (value) => {
+        AsyncStorage.setItem('progressionStep', value);
+        this.setState({ 'progressionStep': value });
+    }
+    render() {
 
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Jeu - Vous êtes le héro !</Text>
-        <DisplayComponent step={this.findById(this.getCurrentId())}></DisplayComponent>
-        {this.findById(this.getCurrentId()).answers.map((object, i) => {
-          return <ActionComponent step={object} key={i} index={i} handler={this.handler} ></ActionComponent>
-        })}
-      </View>
-    )
-  }
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{this.state.progressionStep}</Text>
+                <Text style={styles.text}>Jeu - Vous êtes le héro !</Text>
+                <DisplayComponent step={this.findById(this.getCurrentId())}></DisplayComponent>
+                {this.findById(this.getCurrentId()).answers.map((object, i) => {
+                    return <ActionComponent step={object} key={i} index={i} handler={this.handler} ></ActionComponent>
+                })}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#202020',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#fff'
-  },
-  text: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 30
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#202020',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#fff'
+    },
+    text: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 30
+    }
 })
